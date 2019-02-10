cac2810aecab4373ea5f590555ce39e399ea8c70
René Nitzsche <rene@system25.de>
2019-02-10 17:35:01 +0100

    Translations

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Jarvis Philips Hue
 Control your Philips Hue lights with jarvis.
 
+## first steps
+* after install edit config.ini and configure IP of Hue bridge
+* press link button
+* within 30 seconds say command **light state** to jarvis
+
+Always name your lights will lower characters
+
+## Einrichtung
+* Nach der Installation in die config.ini die IP der Hue bridge eintragen
+* Link-Button an der Bridge drücken
+* Innerhalb von 30 Sekunden das Kommando "Lampenstatus" sagen
+
+Die Namen der Leuchten immer mit Kleinbuchstaben benennen, damit sie gefunden werden können.
--- classes/hue.py
+++ classes/hue.py
@@ -9,8 +9,13 @@ import os
 import ConfigParser
 
 class Hue:
+    lang = None
+    config = None
     bridge = None
-    def __init__(self, bridgeIp):
+    def __init__(self, config, lang):
+        self.config = config
+        self.lang = lang
+        bridgeIp = config.get('hue','ip')
         bridge = self._register_with_bridge(bridgeIp)
         if isinstance(bridge, basestring):
             raise ValueError(bridge)
@@ -24,70 +29,88 @@ class Hue:
             i = i + 1
             try:
                 bridge = Bridge(ip)
-            except PhueRegistrationException as pe:
+            except PhueRegistrationException:
                 time.sleep(1)
                 continue
             except Exception as e:
                 return str(e)
             else:
                 break
-        return bridge if bridge is not None else "Verbindung zur Bridge fehlgeschlagen. Drücke den Link Button!"
-#        return bridge if bridge is not None else "Connection to bridge failed. Press link button!"
-    
-    
-    
+        return bridge if bridge is not None else self._label('error_connect')
+    def _label(self, label):
+        return self.config.get('lang_'+self.lang, label)
+        
     def statusAll(self):
         lights = self.bridge.get_light_objects()
         out = '';
         for light in lights:
-            out = out + "Die Leuchte " + light.name + " ist " + ("an" if light.on else "aus") +". "
+            out = out + self._label('lightstate').format(light.name, (self._label('state_on') if light.on else self._label('state_off'))) + " "
         return out.strip()
 
     def status(self, lightName):
         light = self.__get_light(lightName)
-        if lightName is None:
-            return "Light "+lightName+" not found"
-        return "Die Leuchte " + light.name + " ist " + ("an" if light.on else "aus")
+        if light is None:
+            return self._label('light_not_found').format(lightName)
+        return self._label('lightstate').format(light.name, (self._label('state_on') if light.on else self._label('state_off')))
+
+    def lights_off(self):
+        lights = self.bridge.get_light_objects()
+        for light in lights:
+            self.turn_off(light)
+        return self._label('done')
+
+    def lights_on(self):
+        lights = self.bridge.get_light_objects()
+        for light in lights:
+            self.turn_on(light)
+        return self._label('done')
+
     def turn_off(self, lightName):
         return self.turn_onoff(lightName, False)
     def turn_on(self, lightName):
         return self.turn_onoff(lightName, True)
 
     def turn_onoff(self, lightName, onOrOff):
-        light = self.__get_light(lightName)
+        light = self.__get_light(lightName) if isinstance(lightName, basestring) else lightName
         if light is None:
-            return "Light "+lightName+" not found"
+            return self._label('light_not_found').format(lightName)
         light.on = onOrOff
-        return "Erledigt"
+        return self._label('done')
 
     def random(self, lightName):
         light = self.__get_light(lightName)
         if lightName is None:
-            return "Light "+lightName+" not found"
+            return self._label('light_not_found').format(lightName)
         light.on = True
         light.xy = [random.random(),random.random()]
-        return "Erledigt"
+        return self._label('done')
 
     def darker(self, lightName):
         return self.brightness(lightName, -40)
+
     def brighter(self, lightName):
         return self.brightness(lightName, 40)
+    
     def brightness(self, lightName, change):
         light = self.__get_light(lightName)
         if lightName is None:
-            return "Light "+lightName+" not found"
+            return self._label('light_not_found').format(lightName)
         light.on = True
         light.brightness = light.brightness + change
-        return "Erledigt"
+        return self._label('done')
+
     def __get_light(self, lightName):
         lights = self.bridge.get_light_objects('name')
         if lightName in lights:
             return lights[lightName]
         return None
+
     def do_command(self, command, light):
         commands = {
             'status': self.status,
             'statusall': self.statusAll,
+            'lightson': self.lights_on,
+            'lightsoff': self.lights_off,
             'turnon': self.turn_on,
             'turnoff': self.turn_off,
             'random': self.random,
@@ -110,7 +133,7 @@ if __name__ == "__main__":
     config = ConfigParser.ConfigParser()
     config.read(dir_path + '/../config.ini')
     try:
-        x = Hue(config.get('hue','ip'));
+        x = Hue(config, args.lang);
         print x.do_command(args.command , args.light)
     except Exception as e:
         print e
